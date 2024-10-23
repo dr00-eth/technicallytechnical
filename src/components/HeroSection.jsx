cc8967c71c3abaef2f7034406aeb9d64d4203119
Andrew Meyer <andrewmeyer23@gmail.com>
2024-10-23 10:04:38 -0700

    remove 3rd heading

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -51,9 +51,6 @@ const HeroSection = ({ scrollToSection, aboutRef }) => {
           <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 leading-tight text-yellow-300">
             Where Technology Gets... Technical!
           </h2>
-          <p className="text-xl sm:text-2xl mb-8 text-white">
-            Making AI & Automation as Funny as They Sound. Trust Us, It's Possible!
-          </p>
           <button
             onClick={() => scrollToSection(aboutRef)}
             className="bg-yellow-400 text-blue-600 font-bold py-3 px-6 rounded-full transition duration-300 transform hover:scale-105 hover:bg-yellow-300"
