911b032b0f25f526e085f6bff2bf7022ab67d7a7
Andrew Meyer <andrewmeyer23@gmail.com>
2024-10-23 11:11:07 -0700

    SMS Number

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,9 +22,9 @@ const Footer = () => {
           <div className="w-full md:w-1/4 mb-6 md:mb-0">
             <h4 className="text-lg font-semibold mb-4">Contact Us (If You Dare)</h4>
             <p className="text-gray-400">Email: wizards@technicallytechnical.com</p>
-            <p className="text-gray-400">Phone: (123) 456-7890 (Not a binary joke, we swear)</p>
+            <p className="text-gray-400">Text: 844-787-8232</p>
           </div>
-          <div className="w-full md:w-1/4">
+          {/* <div className="w-full md:w-1/4">
             <h4 className="text-lg font-semibold mb-4">Follow Our Digital Footprints</h4>
             <div className="flex space-x-4">
               <a href="#" className="text-gray-400 hover:text-white transition duration-300"><Facebook size={24} /></a>
@@ -32,7 +32,7 @@ const Footer = () => {
               <a href="#" className="text-gray-400 hover:text-white transition duration-300"><Linkedin size={24} /></a>
               <a href="#" className="text-gray-400 hover:text-white transition duration-300"><Instagram size={24} /></a>
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="mt-8 pt-8 border-t border-gray-700 text-center text-gray-400">
           <p>&copy; {new Date().getFullYear()} Technically Technical - You've technically reached the end... or is it just the beginning?</p>
